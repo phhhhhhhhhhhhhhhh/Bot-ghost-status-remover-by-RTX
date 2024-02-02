0ac2f0623eccaaf2573367fb4a911360acdc26ef
phhhhhhhhhhhhhhhh <158544022+phhhhhhhhhhhhhhhh@users.noreply.github.com>
2024-02-02 01:58:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Assistindo xvideos","Ouvindo Spotify","Lulzando"];
+const statusMessages = ["Assistindo: xvideos","Ouvindo Spotify","Lulzando"];
 
 
 let currentIndex = 0;
